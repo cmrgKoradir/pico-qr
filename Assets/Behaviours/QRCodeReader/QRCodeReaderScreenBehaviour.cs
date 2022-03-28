07af6de418aab55b3f0a25d35e879c6626a55c84
Roger Koradi <roger.koradi@comerge.net>
2022-03-28 13:51:54 +0200

    Seethrough setup

---
+++ Assets/Behaviours/QRCodeReader/QRCodeReaderScreenBehaviour.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Behaviours.QRCodeReader
+{
+    public class QRCodeReaderScreenBehaviour : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+    }
+}
